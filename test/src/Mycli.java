64f8d460ffce9cb369faaf55b881cbc8b0f31a5d
ipt <fashengzxf@outlook.com>
2022-05-01 16:15:48 +0800

    定长

--- test/src/Mycli.java
+++ test/src/Mycli.java
@@ -12,50 +12,41 @@ import java.util.Scanner;
 客户端socket
  */
 public class Mycli {
-    //建立连接
-    public static void main(String[] args) {
-    Socket socket;
-    {
-        try {
-            socket = new Socket("localhost", 9999);
-            //发送数据
-            OutputStream os = socket.getOutputStream();
-            //获取输入
-            Scanner scan = new Scanner(System.in);
-            String msg = scan.nextLine();
-            //写数据
-            os.write(msg.getBytes(StandardCharsets.UTF_8));
-            //接受数据
-            int i,cn = 1,iRes;
-            InputStream is = socket.getInputStream();
-            //读数据
-            byte[] recvBuf = new byte[1024];
-            do {
-                //接收服务器发的信息
-                iRes = is.read(recvBuf);
-                if (iRes > 0) {
-                    System.out.println("\nRecv" + iRes + "bytes");
-                    String encoded = Base64.getEncoder().encodeToString(recvBuf);//base64encode
-                    byte[] decode = Base64.getDecoder().decode(encoded);
-                    System.out.println(new String(decode));
-                } else {
-                    if (iRes == -1) {
-                        System.out.println("关闭连接");
-                        break;
-                    }
-                }
+
+    public static void main(String[] args) throws IOException {
+        Socket socket = new Socket("127.0.0.1", 9999);
+        int cn = 1, iRes;
+        System.out.println("get data from server");
+        InputStream inputStream = socket.getInputStream();
+        byte[] bytes = new byte[111];
+        int leftlength = 111 * 50;
+        int i = 1;
+        while (leftlength > 0) {
+            iRes = inputStream.read(bytes);
+            if (iRes > 0) {
+                System.out.println("\n NO." + i + "收到" + iRes + "bytes:");
+                i++;
+                String encode = Base64.getEncoder().encodeToString(bytes);
+                byte[] decode = Base64.getDecoder().decode(encode);
+                System.out.println(new String(decode));
+                leftlength = leftlength - iRes;
+                System.out.println(leftlength);
+            } else if (iRes == -1) {
+                System.out.println("连接关闭");
+                break;
             }
-                while (iRes > 0);
-                    OutputStream os1 = socket.getOutputStream();
-                    os1.write(msg.getBytes());
-                    os1.flush();
-                    os1.close();
-                    is.close();
 
-
-        } catch (IOException e) {
-            e.printStackTrace();
         }
+        System.out.println("发送数据");
+        OutputStream outputStream = socket.getOutputStream();
+        System.out.println("请输入发送数据");
+        Scanner scan = new Scanner(System.in);
+        String msg = scan.nextLine();
+        outputStream.write(msg.getBytes(StandardCharsets.UTF_8));
+        outputStream.flush();
+        outputStream.close();
+        System.out.println("发送完毕");
+        //关闭socket
+        socket.close();
     }
-}
 }
